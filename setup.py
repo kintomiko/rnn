da9752e6aaccad2b8206a910e21a017d702e451e
Kin Lin <yuanlin1110@gmail.com>
2016-10-18 00:18:48 +1300

    add dependency for openshift

--- setup.py
+++ setup.py
@@ -7,5 +7,4 @@ setup(name='bella-chat', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-		install_requires=['web', 'numpy'],
 	)
